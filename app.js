2618a17b96c91b54e591d28fb2848b559b813b3b
ev2900 <ev2900@gmail.com>
2015-12-04 23:25:22 -0500

    working on routes

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var app = express();
 //Data Base
 mongoose.connect('mongodb://chris:everest2900@ds059644.mongolab.com:59644/cmpsc_497');
 
+//View Setup
+app.set('views', path.join(__dirname + '/views'));
+
 //Static Directory
 app.use(express.static(__dirname + '/public'));
 
