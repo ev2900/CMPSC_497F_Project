1fcf700c5b00761f0191860295979e78df5579ab
ev2900 <ev2900@gmail.com>
2015-12-05 14:49:49 -0500

    adding a new route

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,19 @@ router.get('/', function(req, res, next) {
 	res.render('index');
 });
 
+
+
 router.get('/healthcheck', function(req, res) {
 	var responseObject = { message: 'OK' };
 	res.send(responseObject);
 });
+
+router.get('/like/:icecreamchoice', function(req, res) {
+	var choice = req.params.icecreamchoice;
+	var responseObject = {message: 'I like' + choice + 'too!'};
+	res.send(responseObject);
+});
+
+
 
 module.exports = router;
