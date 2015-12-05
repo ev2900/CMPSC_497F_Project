725d5bc7f08367f22d37a5b1577ca2a26773835d
ev2900 <ev2900@gmail.com>
2015-12-05 14:43:28 -0500

    added a health check restful api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
 	res.render('index');
 });
 
+router.get('/healthcheck', function(req, res) {
+	var responseObject = { message: 'OK' };
+	res.send(responseObject);
+});
+
 module.exports = router;
