d5cae59f245bd9fdd81abea90f66df0539cc11a3
ev2900 <ev2900@gmail.com>
2015-12-04 23:33:17 -0500

    working on routes via hbs

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://chris:everest2900@ds059644.mongolab.com:59644/cmpsc_
 
 //View Setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'hbs')
+app.set('view engine', 'hbs')
 
 //Static Directory
 app.use(express.static(path.join(__dirname, 'public')));
