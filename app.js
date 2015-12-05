03772cd10d5e35557cf7f5422af3a884abad591a
ev2900 <ev2900@gmail.com>
2015-12-04 22:39:16 -0500

    added a static directory

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-//var app = angular.module('QuadreplexApp', []);
-
 var express = require("express");
 var app     = express();
 var path    = require("path");
 
+//Static Directory
+app.use(express.static(__dirname + '/public'));
 
+//Home Page
 app.get('/',function(req,res){
+
   res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 app.listen(3000);
