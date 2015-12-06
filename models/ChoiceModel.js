bbda1c90bba8eaf091a64d8e502833191c571cd0
ev2900 <ev2900@gmail.com>
2015-12-05 23:56:13 -0500

    update

--- models/ChoiceModel.js
+++ models/ChoiceModel.js
@@ -1,11 +1,13 @@
 var mongoose = require('mongoose');
-var userService = require('../services/index-service');
-var mySchema = mongoose.Schema({
-  input_genome: String, 
-  gff_file: String
+var Schema = mongoose.Schema;
+var indexService = require('../services/index-service');
+
+var indexSchema = new Schema({
+  input_genome: {type:String, require: 'Please enter a genome'}, 
+  gff_file: {type:String, require: 'Pleae enter a gff'}
 });
 
-var ChoiceModel = mongoose.model('ChoiceModel', mySchema);
+var ChoiceModel = mongoose.model('ChoiceModel', indexSchema);
 
 module.exports = {
   ChoiceModel: ChoiceModel
